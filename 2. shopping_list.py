98bc699aa6d27f55bc7148f9785797e672d9eb94
willdomkahari <developer.wadu@gmail.com>
2024-10-13 22:46:30 +0200

    lecture 2

---
+++ 2. shopping_list.py
@@ -0,0 +1,15 @@
+# Number 1
+apples = 5
+bananas = 2
+bread = 1
+milk = 2
+
+# Number 2
+total_items = apples + bananas + bread + milk
+print("Total number of items:", total_items)
+
+# Number 3
+print("Apples:", apples)
+print("Bananas:", bananas)
+print("Bread:", bread, "loaf")
+print("Milk:", milk, "liters")
