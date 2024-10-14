4e25e4dcd097cba0195d0517fd3ac258fe1db535
willdomkahari <developer.wadu@gmail.com>
2024-10-14 23:20:34 +0200

    - added comparison to inputs to declare variables as boolean - added strip to clean data

--- 5a. eligibility_for_benefits.py
+++ 5a. eligibility_for_benefits.py
@@ -1,14 +1,14 @@
 #1. Define variable age, is_student, and is_employed
 age = int(input("Enter your age: "))
-is_student= input("Are you a student? Yes or No: ").capitalize()
-is_employed = input("Are you employed? Yes or No: ").capitalize()
+is_student= input("Are you a student? Yes or No: ").strip().capitalize() == "Yes"
+is_employed = input("Are you employed? Yes or No: ").strip().capitalize() == "Yes"
 
 #2. Using logical operators (and, or) determine eligibility for different benefits
-if is_student == "Yes" and age < 25:
+if is_student and age < 25:
     print("Eligible for student discount.")
-elif is_employed == "Yes" and 18 < age < 65:
+elif is_employed and 18 < age < 65:
     print("Eligible for work benefits.")
-elif is_student == "Yes" or is_employed == "Yes":
+elif is_student or is_employed:
     print("Eligible for general benefits.")
 else:
     print("Not eligible for general benefits.")
--- 5b. event_access.py
+++ 5b. event_access.py
@@ -1,14 +1,14 @@
 # 1. Define variables age, is_member, and has_invitation
 age = int(input("Enter your age:"))
-is_member = input("Are you a member of this club? Yes or No: ").lower()
-has_invitation = input("Do you have an invitation? Yes or No: ").lower()
+is_member = input("Are you a member of this club? Yes or No: ").strip().capitalize() == "Yes"
+has_invitation = input("Do you have an invitation? Yes or No: ").strip().capitalize() == "Yes"
 
 # 2. Using logical operators (and, or), determine access to different events
-if is_member == "yes" and age >= 18:
+if is_member and age >= 18:
     print("Access to Member's Event.")
-elif is_member == "yes" and has_invitation == "yes":
+elif is_member and has_invitation == "yes":
     print("Access to Exclusive Event.")
-elif has_invitation == "yes" or age >= 21:
+elif has_invitation or age >= 21:
     print("Access to VIP Event.")
 else:
     print("Access to General Event.")
